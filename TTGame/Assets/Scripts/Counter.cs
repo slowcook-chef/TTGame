bc452d172b92f98260b3bc2aa518d0339f8f07bb
Tomas Rodriguez <trodri@slowcook.games>
2025-01-13 13:44:00 -0500

    Add: Splines package, and basic counter functionality

--- TTGame/Assets/Scripts/Counter.cs
+++ TTGame/Assets/Scripts/Counter.cs
@@ -1,16 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
+    [SerializeField] private int count = 0;
+    [SerializeField] private TMPro.TextMeshProUGUI text;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        text.text = count.ToString();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Increment()
     {
-        
+        text.text = count++.ToString();
     }
 }
